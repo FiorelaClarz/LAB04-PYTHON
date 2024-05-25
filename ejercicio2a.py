7da40d6d86084621958c0355fac5528516930ff9
Fiorela <clafiorelariza@gmail.com>
2024-05-25 11:57:16 -0500

    usando el metodo join y negative en knight

--- ejercicio2a.py
+++ ejercicio2a.py
@@ -1,4 +1,8 @@
 from interpreter import draw
 from chessPictures import *
 
-draw(pawn)
+fila_1 = knight.join(knight.negative())
+fila_2 = fila_1.negative()
+# añadiendo las filas en resultado
+resultado = fila_1.under(fila_2)
+draw(resultado)
