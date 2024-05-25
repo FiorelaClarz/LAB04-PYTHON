c1d2749c8778759ebf7c62ed2c294bccb06d9c8a
Fiorela <clafiorelariza@gmail.com>
2024-05-25 10:36:45 -0500

    añadiendo las nuevas modificaciones en colors.py, interpreter.py, Picture.´py y pieces.py de acuerdo al repositorio del lab

--- Picture.py
+++ Picture.py
@@ -1,35 +1,54 @@
+from colors import *
 class Picture:
-    def __init__(self, img):
-        self.img = img
+  def __init__(self, img):
+    self.img = img;
 
-    def _invColor(self, char):
-        inverse_colors = {
-            ' ': '#',  # Ejemplo, se deben añadir todos los mapeos necesarios según el archivo colors.py
-            '#': ' ',
-            # Añadir otros colores y sus inversos
-        }
-        return inverse_colors.get(char, char)
+  def __eq__(self, other):
+    return self.img == other.img
 
-    def verticalMirror(self):
-        return Picture([line[::-1] for line in self.img])
+  def _invColor(self, color):
+    if color not in inverter:
+      return color
+    return inverter[color]
 
-    def horizontalMirror(self):
-        return Picture(self.img[::-1])
+  def verticalMirror(self):
+    """ Devuelve el espejo vertical de la imagen """
+    vertical = []
+    for value in self.img:
+    	vertical.append(value[::-1])
+    return vertical
 
-    def negative(self):
-        return Picture([''.join(self._invColor(char) for char in line) for line in self.img])
+  def horizontalMirror(self):
+    """ Devuelve el espejo horizontal de la imagen """
+    return Picture(None)
 
-    def join(self, other):
-        return Picture([self_line + other_line for self_line, other_line in zip(self.img, other.img)])
+  def negative(self):
+    """ Devuelve un negativo de la imagen """
+    return Picture(None)
 
-    def up(self, other):
-        return Picture(other.img + self.img)
+  def join(self, p):
+    """ Devuelve una nueva figura poniendo la figura del argumento 
+        al lado derecho de la figura actual """
+    return Picture(None)
 
-    def under(self, other):
-        return Picture(self.img + other.img)
+  def up(self, p):
+    return Picture(None)
 
-    def horizontalRepeat(self, n):
-        return Picture([line * n for line in self.img])
+  def under(self, p):
+    """ Devuelve una nueva figura poniendo la figura p sobre la
+        figura actual """
+    return Picture(None)
+  
+  def horizontalRepeat(self, n):
+    """ Devuelve una nueva figura repitiendo la figura actual al costado
+        la cantidad de veces que indique el valor de n """
+    return Picture(None)
 
-    def verticalRepeat(self, n):
-        return Picture(self.img * n)
+  def verticalRepeat(self, n):
+    return Picture(None)
+
+  #Extra: Sólo para realmente viciosos 
+  def rotate(self):
+    """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
+    o antihorario"""
+    return Picture(None)
--- colors.py
+++ colors.py
@@ -1,5 +1,21 @@
-inverse_colors = {
-    ' ': '#',
-    '#': ' ',
-    # Añadir otros colores y sus inversos
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHTGRAY = (200, 200, 200)
+GRAY = (127, 127, 127)
+DARKGRAY = (50, 50, 50)
+BLUE = (0, 0, 255)
+
+color = {
+  '_': LIGHTGRAY,
+  '=': GRAY,
+  '.': WHITE,
+  '@': BLACK,
+  '#': DARKGRAY,
+  ' ': BLUE,
+}
+inverter = {
+  '_': '=',
+  '=': '_',
+  '.': '@',
+  '@': '.',
 }
