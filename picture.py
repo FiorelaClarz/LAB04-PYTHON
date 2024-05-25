fdfd36c3ee17da8430b0a5cce42151b05b17cedd
Fiorela <clafiorelariza@gmail.com>
2024-05-25 11:54:40 -0500

    modificando los metodos con los requerimientos del ejercico

--- picture.py
+++ picture.py
@@ -1,9 +1,9 @@
 from colors import *
 class Picture:
-  def __init__(self, img):
+  def _init_(self, img):
     self.img = img;
 
-  def __eq__(self, other):
+  def _eq_(self, other):
     return self.img == other.img
 
   def _invColor(self, color):
@@ -13,39 +13,65 @@ class Picture:
 
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
-    vertical = []
-    for value in self.img:
-    	vertical.append(value[::-1])
-    return vertical
+    new_img=[]
+    for row in self.img:
+      new_img.append(row[::-1])
+    return Picture(new_img)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    return Picture(None)
+    new_img=self.img[::-1]
+    return Picture(new_img)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    return Picture(None)
+    negative_img=[]
+    for row in self.img:
+      negative_row=""
+      for char in row:
+        negative_char=self._invColor(char)
+        negative_row+=negative_char
+      negative_img.append(negative_row)
+    return Picture(negative_img)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
+    new_img=[]
+    numFilas=len(self.img)
+    for i in range(numFilas):
+      fila=self.img[i]+p.img[i]
+      new_img.append(fila)
+    return Picture(new_img)
 
   def up(self, p):
-    return Picture(None)
+    new_img=[]
+    for row in p.img:
+      new_img.append(row)
+    for row in self.img:
+      new_img.append(row)
+    return Picture(new_img)
 
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
         figura actual """
-    return Picture(None)
+    new_img=[]
+    for row in self.img:
+      new_img.append(row)
+    for row in p.img:
+      new_img.append(row)
+    return Picture(new_img)
   
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    new_img=[]
+    for row in self.img:
+      new_img.append(row*n)
+    return Picture(new_img)
 
   def verticalRepeat(self, n):
-    return Picture(None)
+    return Picture(self.img*n)
 
   #Extra: Sólo para realmente viciosos 
   def rotate(self):
